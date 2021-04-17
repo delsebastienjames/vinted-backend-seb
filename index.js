0166365c4136b5b917f1952d3f147cbc58fb171d
Sebastien <delsebastienjames@gmail.com>
2021-04-17 08:45:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cloudinary = require("cloudinary").v2;
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+//const isAuthenticated = require("./middlewares/isAuthenticated");
+
+// Connexion du serveur et de la base de données (BDD)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true, // unique : true du model User
+    //useFindAndModify: false,
+});
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Importer les routes
+const userRoutes = require("./routes/user"); // fichier routes user.js
+const offerRoutes = require("./routes/offer"); // fichier routes offer.js
+app.use(userRoutes);
+app.use(offerRoutes);
+
+app.all("*", (req, res) => {
+    res.status(404).json({ error: "Cette route n'existe pas." });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server Started");
+});

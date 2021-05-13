957a7e6930450fa11380ba67ef643da74e1c35e0
Sebastien <delsebastienjames@gmail.com>
2021-05-13 16:36:54 +0200

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -12,17 +12,20 @@ app.use(cors());
 
 app.post("/payment", async (req, res) => {
   try {
+    // Réception du token créer via l'API Stripe depuis le Frontend
+    const stripeToken = req.fields.stripeToken;
+    // Créer la transaction
     // Envoyer le token à l'API Stripe
-    let { status } = await stripe.charges.create({
+    const response = await stripe.charges.create({
       amount: req.fields.amount * 100,
       currency: "eur",
       description: `Paiement vinted pour : ${req.fields.title}`,
-      source: req.fields.token,
+      source: stripeToken,
     });
     // Recevoir une réponse de l'API Stripe
     //console.log(response);
 
-    res.json({ status });
+    res.json({ response });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ message: error.message });

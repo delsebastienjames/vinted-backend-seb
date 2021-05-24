bceb289d96ac722b32a5e9b4094c7dea8197c12a
Sebastien <delsebastienjames@gmail.com>
2021-05-24 09:18:42 +0200

    Update isAuthenticated.js

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,27 +2,27 @@
 //const Offer = require("../models/Offer");
 const User = require("../models/User");
 
-const isAuthenticated = async(req, res, next) => {
-    try {
-        if (req.headers.authorization) {
-            const token = req.headers.authorization.replace("Bearer ", "");
-            //   console.log(token);
-            const user = await User.findOne({ token: token }).select("account_id");
-            if (user) {
-                // ajouter à req l'objet user
-                req.user = user;
-                next();
-            } else {
-                // Sinon ====> unauthorized
-                res.status(401).json({ message: "Unauthorized" });
-            }
-        } else {
-            // Sinon ====> unauthorized
-            res.status(401).json({ message: "Unauthorized" });
-        }
-    } catch (error) {
-        res.status(401).json({ error: error.message });
+const isAuthenticated = async (req, res, next) => {
+  try {
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.replace("Bearer ", "");
+      //   console.log(token);
+      const user = await User.findOne({ token: token }).select("account_id");
+      if (user) {
+        // ajouter à req l'objet user
+        req.user = user;
+        next();
+      } else {
+        // Sinon ====> unauthorized
+        res.status(401).json({ message: "Unauthorized" });
+      }
+    } else {
+      // Sinon ====> unauthorized
+      res.status(401).json({ message: "Unauthorized" });
     }
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
 };
 
 module.exports = isAuthenticated;
